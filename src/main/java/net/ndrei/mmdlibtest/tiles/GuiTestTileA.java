2c08d65ecb446831b91e3c4de36830c9c5c42a06
faceofcat <candreix@gmail.com>
2018-02-13 22:56:30 +0200

    gui system... take 6... starting the json gui loading

--- src/main/java/net/ndrei/mmdlibtest/tiles/GuiTestTileA.java
+++ src/main/java/net/ndrei/mmdlibtest/tiles/GuiTestTileA.java
@@ -3,16 +3,19 @@ package net.ndrei.mmdlibtest.tiles;
 import java.awt.Color;
 import javax.annotation.Nonnull;
 import com.mcmoddev.lib.container.PlayerInventory;
-import com.mcmoddev.lib.container.gui.FeatureWrapperGui;
 import com.mcmoddev.lib.container.gui.GuiContext;
 import com.mcmoddev.lib.container.gui.IWidgetGui;
-import com.mcmoddev.lib.container.gui.layout.GridLayout;
-import com.mcmoddev.lib.container.gui.layout.VerticalStackLayout;
-import com.mcmoddev.lib.container.gui.util.Padding;
+import com.mcmoddev.lib.container.gui.layout.CanvasLayout;
+import com.mcmoddev.lib.container.parser.IWidgetGuiParser;
+import com.mcmoddev.lib.container.parser.WidgetParser;
 import com.mcmoddev.lib.feature.FluidTankFeature;
 import com.mcmoddev.lib.feature.ItemInventoryFeature;
 import com.mcmoddev.lib.feature.PlayerInventoryFeature;
 import com.mcmoddev.lib.tile.MMDFeaturesTileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.ndrei.mmdlibtest.MMDLibTest;
 
 public class GuiTestTileA extends MMDFeaturesTileEntity {
     @Override
@@ -35,28 +38,30 @@ public class GuiTestTileA extends MMDFeaturesTileEntity {
         this.addFeature(new PlayerInventoryFeature(PlayerInventory.QUICKBAR, 9));
     }
 
+    private IWidgetGuiParser cachedWidgetGui = null;
+
     @Override
     @Nonnull
+    @SideOnly(Side.CLIENT)
     public IWidgetGui getRootWidgetGui(@Nonnull GuiContext context) {
-        return new VerticalStackLayout()
-            .addPiece(new GridLayout(9, 3)
-                .addPiece(new FeatureWrapperGui(context, this, "inputs_1"), 0, 1, 2, 2)
-                .addPiece(new FeatureWrapperGui(context, this, "inputs_2"), 3, 1, 3, 2)
-                .addPiece(new FeatureWrapperGui(context, this, "inputs_3"), 7, 1, 2, 2)
-                .addPiece(new FeatureWrapperGui(context, this, "fluid_a"), 2, 0, 1, 3)
-                .addPiece(new FeatureWrapperGui(context, this, "fluid_b"), 6, 0, 1, 3)
-            )
-            .addPiece(new FeatureWrapperGui(context, this, "player_quickbar")
-                .setPadding(Padding.top(7))
-            )
-//            .addPiece(new HorizontalStackLayout()
-//                .addPiece(new TextButtonWidgetGui("Add Lava", 75))
-//                .addPiece(new TextButtonWidgetGui("Add Water", 75))
-//                .addPiece(new TextButtonWidgetGui("Clear Fluids", 75))
-//                .setPadding(Padding.top(7))
+        if (this.cachedWidgetGui == null) {
+            this.cachedWidgetGui = WidgetParser.parseRootPiece(null,
+                new ResourceLocation(MMDLibTest.MODID, "guitesta")
+            );
+        }
+
+        IWidgetGui gui = (this.cachedWidgetGui == null) ? null : this.cachedWidgetGui.createRootWidgetGui(context);
+        return (gui == null) ? new CanvasLayout() : gui;
+//        return new VerticalStackLayout()
+//            .addPiece(new GridLayout(9, 3)
+//                .addPiece(new FeatureWrapperGui(context, this, "inputs_1"), 0, 1, 2, 2)
+//                .addPiece(new FeatureWrapperGui(context, this, "inputs_2"), 3, 1, 3, 2)
+//                .addPiece(new FeatureWrapperGui(context, this, "inputs_3"), 7, 1, 2, 2)
+//                .addPiece(new FeatureWrapperGui(context, this, "fluid_a"), 2, 0, 1, 3)
+//                .addPiece(new FeatureWrapperGui(context, this, "fluid_b"), 6, 0, 1, 3)
 //            )
-//            .addPiece(new TextboxPiece(200))
-//            .addPiece(new TextboxPiece(200, 5))
-            ;
+//            .addPiece(new FeatureWrapperGui(context, this, "player_quickbar")
+//                .setPadding(Padding.top(7))
+//            );
     }
 }
--- src/main/java/net/ndrei/mmdlibtest/tiles/GuiTestTileB.java
+++ src/main/java/net/ndrei/mmdlibtest/tiles/GuiTestTileB.java
@@ -2,12 +2,13 @@ package net.ndrei.mmdlibtest.tiles;
 
 import java.util.Arrays;
 import java.util.List;
-import com.mcmoddev.lib.MMDLib;
 import com.mcmoddev.lib.container.gui.GuiContext;
 import com.mcmoddev.lib.container.gui.IWidgetGui;
+import com.mcmoddev.lib.container.gui.LabelWidgetGui;
 import com.mcmoddev.lib.container.gui.TextButtonWidgetGui;
-import com.mcmoddev.lib.container.gui.TextWidgetGui;
+import com.mcmoddev.lib.container.gui.TextEditWidgetGui;
 import com.mcmoddev.lib.container.gui.layout.HorizontalStackLayout;
+import com.mcmoddev.lib.container.gui.layout.VerticalStackLayout;
 import com.mcmoddev.lib.container.gui.util.Padding;
 import com.mcmoddev.lib.container.widget.ActionWidget;
 import com.mcmoddev.lib.container.widget.DataWidget;
@@ -17,12 +18,20 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class GuiTestTileB extends MMDTileEntity {
-    private DataWidget dataWidget = new DataWidget("data") {
+    @SuppressWarnings("WeakerAccess")
+    private class Data extends DataWidget {
         private int someValue;
         private float someFloatValue;
 
-        @DataField
-        public String someOtherValue;
+        @DataField public String someOtherValue;
+
+        @DataField public String textA;
+        @DataField public String textB;
+        @DataField public String textC;
+
+        Data(String key) {
+            super(key);
+        }
 
         @DataGetter
         public int getSomeValue() { return this.someValue; }
@@ -35,38 +44,85 @@ public class GuiTestTileB extends MMDTileEntity {
 
         @DataSetter("floatValue")
         public void setSomeFloat(float value) { this.someFloatValue = value; }
-    };
+    }
+
+    private Data dataWidget = new Data("data");
 
     @Override
     public List<IWidget> getWidgets(GuiContext context) {
         return Arrays.asList(
             this.dataWidget,
+            this.dataWidget.getStringUpdateActionWidget("data_textA", "textA"),
+            this.dataWidget.getStringUpdateActionWidget("data_textC", "textC"),
             new ActionWidget("action")
-                .setClientSideConsumer(() -> MMDLib.logger.info("Hello from client side!"))
-                .setServerSideConsumer(() -> {
-                    MMDLib.logger.info("Hello from server side!");
-                    //noinspection ConstantConditions
-                    this.dataWidget.setValue("someValue",
-                        this.dataWidget.getValue(Integer.class, "someValue") + 42);
-            })
+                .setServerSideConsumer(() -> this.dataWidget.setSomeValue(this.dataWidget.getSomeValue() + 42))
         );
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public IWidgetGui getRootWidgetGui(GuiContext context) {
-        HorizontalStackLayout layout = new HorizontalStackLayout();
 
-        layout.addPiece(new TextButtonWidgetGui("Click me!")
-            .setClickAction(() -> MMDLib.logger.info("Hello from GUI side!"))
-            .connectToWidget("action")
-        );
+        return new VerticalStackLayout() {{
+            addPiece(new TextEditWidgetGui(100)
+                .connectActionWidget("data_textA")
+                .setPadding(Padding.bottom(5))
+            );
+            addPiece(new HorizontalStackLayout() {{
+                    addPiece(new LabelWidgetGui("SERVER: "));
+                    addPiece(new LabelWidgetGui()
+                        .connectDataWidget("data", "textA")
+                    );
+                }}
+                    .setPadding(new Padding(0, 0, 0, 10))
+            );
 
-        layout.addPiece(new TextWidgetGui()
-            .connectDataWidget("data", "someValue")
-            .setPadding(new Padding(10, 0))
-        );
+            addPiece(new TextEditWidgetGui(100)
+                .connectDataWidget("data", "textB")
+                .setMaxLength(100)
+                .setPadding(Padding.bottom(5))
+            );
+            addPiece(new HorizontalStackLayout() {{
+                    addPiece(new LabelWidgetGui("CLIENT: "));
+                    addPiece(new LabelWidgetGui()
+                        .connectDataWidget("data", "textB")
+                    );
+                }}
+                    .setPadding(new Padding(0, 0, 0, 10))
+            );
 
-        return layout;
+            addPiece(new TextEditWidgetGui(100)
+                .connectDataWidget("data", "textC")
+                .connectActionWidget("data_textC")
+                .setMaxLength(100)
+                .setPadding(Padding.bottom(5))
+            );
+            addPiece(new HorizontalStackLayout() {{
+                    addPiece(new LabelWidgetGui("CLIENT: "));
+                    addPiece(new LabelWidgetGui()
+                        .connectDataWidget("data", "textC")
+                    );
+                }}
+                    .setPadding(new Padding(0, 0, 0, 10))
+            );
+            addPiece(new HorizontalStackLayout() {{
+                    addPiece(new LabelWidgetGui("SERVER: "));
+                    addPiece(new LabelWidgetGui()
+                        .connectDataWidget("data", "textC")
+                    );
+                }}
+                    .setPadding(new Padding(0, 0, 0, 10))
+            );
+
+            addPiece(new HorizontalStackLayout() {{
+                addPiece(new TextButtonWidgetGui("Click me!")
+                    .connectToWidget("action")
+                );
+                addPiece(new LabelWidgetGui()
+                    .connectDataWidget("data", "someValue")
+                    .setPadding(new Padding(10, 0))
+                );
+            }});
+        }};
     }
 }
