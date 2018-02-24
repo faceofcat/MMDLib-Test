8b213414194203ee78aa785850ba007fe47c4522
faceofcat <candreix@gmail.com>
2018-02-24 15:35:45 +0200

    gui system... take 7... energy stuff

--- src/main/java/net/ndrei/mmdlibtest/tiles/GuiTestTileA.java
+++ src/main/java/net/ndrei/mmdlibtest/tiles/GuiTestTileA.java
@@ -3,19 +3,20 @@ package net.ndrei.mmdlibtest.tiles;
 import java.awt.Color;
 import javax.annotation.Nonnull;
 import com.mcmoddev.lib.container.PlayerInventory;
+import com.mcmoddev.lib.container.gui.FeatureWrapperGui;
 import com.mcmoddev.lib.container.gui.GuiContext;
 import com.mcmoddev.lib.container.gui.IWidgetGui;
-import com.mcmoddev.lib.container.gui.layout.CanvasLayout;
-import com.mcmoddev.lib.container.parser.IWidgetGuiParser;
-import com.mcmoddev.lib.container.parser.WidgetParser;
+import com.mcmoddev.lib.container.gui.layout.GridLayout;
+import com.mcmoddev.lib.container.gui.layout.VerticalStackLayout;
+import com.mcmoddev.lib.container.gui.util.Padding;
 import com.mcmoddev.lib.feature.FluidTankFeature;
 import com.mcmoddev.lib.feature.ItemInventoryFeature;
 import com.mcmoddev.lib.feature.PlayerInventoryFeature;
 import com.mcmoddev.lib.tile.MMDFeaturesTileEntity;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.init.Items;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.ndrei.mmdlibtest.MMDLibTest;
 
 public class GuiTestTileA extends MMDFeaturesTileEntity {
     public GuiTestTileA() {
@@ -26,46 +27,50 @@ public class GuiTestTileA extends MMDFeaturesTileEntity {
     protected void initFeatures() {
         super.initFeatures();
 
-        this.addFeature(new ItemInventoryFeature("inputs_1", 4))
+        this.addFeature(new ItemInventoryFeature("inputs_1", 4,
+            (slot, stack) -> !stack.isEmpty() && (stack.getItem() == Items.APPLE),
+            (slot, stack) -> true))
             .setColumns(2)
             .setOverlayColor(Color.RED.getRGB(), 42);
-        this.addFeature(new ItemInventoryFeature("inputs_2", 6))
+        this.addFeature(new ItemInventoryFeature("inputs_2", 6,
+            (slot, stack) -> !stack.isEmpty() && (stack.getItem() == Items.BREAD),
+            (slot, stack) -> false))
             .setColumns(3)
             .setOverlayColor(Color.GREEN.getRGB(), 42);
-        this.addFeature(new ItemInventoryFeature("inputs_3", 4))
+        this.addFeature(new ItemInventoryFeature("inputs_3", 4,
+            (slot, stack) -> !stack.isEmpty() && (stack.getItem() == Items.BEEF),
+            (slot, stack) -> true))
             .setColumns(2)
             .setOverlayColor(Color.BLUE.getRGB(), 42);
 
-        this.addFeature(new FluidTankFeature("fluid_a", 5000, null, null));
-        this.addFeature(new FluidTankFeature("fluid_b", 5000, null, null));
+        this.addFeature(new FluidTankFeature("fluid_a", 5000,
+            fluid -> (fluid != null) && (fluid.getFluid() == FluidRegistry.WATER),
+            fluid -> true));
+        this.addFeature(new FluidTankFeature("fluid_b", 5000,
+            fluid -> true,
+            fluid -> false));
 
+        this.addFeature(new PlayerInventoryFeature(PlayerInventory.INVENTORY, 9));
         this.addFeature(new PlayerInventoryFeature(PlayerInventory.QUICKBAR, 9));
     }
-
-    private IWidgetGuiParser cachedWidgetGui = null;
 
     @Override
     @Nonnull
     @SideOnly(Side.CLIENT)
     public IWidgetGui getRootWidgetGui(@Nonnull GuiContext context) {
-        if (this.cachedWidgetGui == null) {
-            this.cachedWidgetGui = WidgetParser.parseRootPiece(null,
-                new ResourceLocation(MMDLibTest.MODID, "guitesta")
+        return new VerticalStackLayout()
+            .addPiece(new GridLayout(9, 3)
+                .addPiece(new FeatureWrapperGui(context, this, "inputs_1"), 0, 1, 2, 2)
+                .addPiece(new FeatureWrapperGui(context, this, "inputs_2"), 3, 1, 3, 2)
+                .addPiece(new FeatureWrapperGui(context, this, "inputs_3"), 7, 1, 2, 2)
+                .addPiece(new FeatureWrapperGui(context, this, "fluid_a"), 2, 0, 1, 3)
+                .addPiece(new FeatureWrapperGui(context, this, "fluid_b"), 6, 0, 1, 3)
+            )
+            .addPiece(new FeatureWrapperGui(context, this, "player_inventory")
+                .setPadding(Padding.top(7))
+            )
+            .addPiece(new FeatureWrapperGui(context, this, "player_quickbar")
+                .setPadding(Padding.top(7))
             );
-        }
-
-        IWidgetGui gui = (this.cachedWidgetGui == null) ? null : this.cachedWidgetGui.createRootWidgetGui(context);
-        return (gui == null) ? new CanvasLayout() : gui;
-//        return new VerticalStackLayout()
-//            .addPiece(new GridLayout(9, 3)
-//                .addPiece(new FeatureWrapperGui(context, this, "inputs_1"), 0, 1, 2, 2)
-//                .addPiece(new FeatureWrapperGui(context, this, "inputs_2"), 3, 1, 3, 2)
-//                .addPiece(new FeatureWrapperGui(context, this, "inputs_3"), 7, 1, 2, 2)
-//                .addPiece(new FeatureWrapperGui(context, this, "fluid_a"), 2, 0, 1, 3)
-//                .addPiece(new FeatureWrapperGui(context, this, "fluid_b"), 6, 0, 1, 3)
-//            )
-//            .addPiece(new FeatureWrapperGui(context, this, "player_quickbar")
-//                .setPadding(Padding.top(7))
-//            );
     }
 }
